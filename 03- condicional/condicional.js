93e28815bd03a628aa1687cd989d1b4521ae8c1e
Alvaro Medina <168005749+alvaroMedina777@users.noreply.github.com>
2026-06-13 19:03:25 -0300

    modifico linea 15, mostraba mal total a pagar

--- 03- condicional/condicional.js
+++ 03- condicional/condicional.js
@@ -7,18 +7,18 @@ Un comerciante hace descuentos a sus clientes de la siguiente forma:
 */
 
 let dolar = 1555;
-let compra = number(prompt("Cuantos dolares compró?"));
+let compra = Number(prompt("Cuantos dolares compró?"));
 
 let resultado = dolar * compra;
 
 if (compra < 100) {
-    alert("No hay descuento, el total a pagar es: " + compra);
+    alert("No hay descuento, el total a pagar es: $" + compra);
 } else if (compra >= 100 && compra <= 300) {
-    alert("Se le aplicará un descuento del 5%, el total a pagar es: " + ((resultado) * 0.95)); 
+    alert("Se le aplicará un descuento del 5%, el total a pagar es: $" + ((resultado) * 0.95)); 
 } else if (compra >= 300 && compra <= 500){
-    alert("Se le aplicará un descuento del 10%, el total a pagar es: " + ((resultado) * 0.90));
+    alert("Se le aplicará un descuento del 10%, el total a pagar es: $" + ((resultado) * 0.90));
 } else {
-    alert("Se le aplicará un descuento del 15%, el total a pagar es: " + ((resultado) * 0.85));
+    alert("Se le aplicará un descuento del 15%, el total a pagar es: $" + ((resultado) * 0.85));
 }
 
-alert("El total a pagar es: " + resultado);
+// console.log("El total a pagar es: " + resultado);
