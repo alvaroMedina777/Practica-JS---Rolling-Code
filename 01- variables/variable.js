d320096d1cb13186a44f0d519153f2da8d5bbd94
Alvaro Medina <168005749+alvaroMedina777@users.noreply.github.com>
2026-06-14 19:04:21 -0300

    creo carpeta y archivo variable.js y termino los ejercicios

---
+++ 01- variables/variable.js
@@ -0,0 +1,34 @@
+/*Ejercicios
+
+1 - Siguiendo buenas prácticas que palabra/s reservada/s de javascript debemos usar para declarar variables. 
+Para declarar variables debemos usar let o const. Let para vatiables que cambiaran su valor y const para variables que tendran un valor fijo.
+
+2 - Declarar e inicializar una variable de tipo String, una de tipo booleana y dos de tipo number.
+
+let nombre = "Alvaro";
+let mayorDeEdad = true;
+let edad = 34;
+let numeroDecimal = 89.7;
+
+3 - ¿Con qué  convención de nomenclatura está escrito el nombre de la siguente variable (pascal case, camel case o snake case)?
+
+let cantidadDeAlumnos=45 camelCase
+
+La variable esta escrita con la convencion de nomenclatura camelCase, ya que la primera palabra comienza con minúscula y las siguientes palabras comienzan con mayúscula.
+
+4 - Qué tipo de datos tienen almacenados las siguientes variables?
+
+ - const MASCOTAS=['Boby','Chatran','Scooby']
+ - let comisionActiva
+ - let estaInactiva= false
+
+La variable MASCOTAS tiene almacenado un array de strings.
+La variable comisionActiva tiene almacenado el valor undefined, ya que no se le ha asignado ningún valor.
+La variable estaInactiva tiene almacenado un valor booleano false.
+
+5 - ¿Cómo puedo saber que tipo de datos tiene una variable desde la consola?. 
+
+Para saber el tipo de datos de una variable desde la consola, se utiliza el operador typeof seguido del nombre de la variable. Por ejemplo, si queremos saber el tipo de datos de la variable MASCOTAS, podemos escribir:
+console.log(typeof MASCOTAS);
+
+*/
