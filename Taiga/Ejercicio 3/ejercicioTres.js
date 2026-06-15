bbb318e20dd5be37751ac02f25b8c09da8a6c36d
Alvaro Medina <168005749+alvaroMedina777@users.noreply.github.com>
2026-06-15 20:24:51 -0300

    termino ejercicio numero tres del taiga

--- Taiga/Ejercicio 3/ejercicioTres.js
+++ Taiga/Ejercicio 3/ejercicioTres.js
@@ -5,3 +5,25 @@
 // Usa condicionales para ejecutar la operación correspondiente.  
 // Muestra el resultado en la consola.  
 // Si la operación no es válida, informa al usuario con un mensaje en la consola.
+
+
+let primerNro = parseFloat(prompt("Ingrese el primer numero decimal"));
+let segundoNro = parseFloat(prompt("Ingrese el segundo numero decimal"));
+
+let operador = parseInt(prompt("1: Suma, 2: Resta, 3: Multiplicacion, 4: Division"));
+
+if (operador == 1) {
+    let resultado = parseFloat(primerNro + segundoNro);
+    console.log("El resultado de la suma es: " + resultado);
+} else if (operador == 2) {
+    let resultado = parseFloat(primerNro - segundoNro);
+    console.log("El resultado de la resta es: " + resultado)
+} else if (operador == 3) {
+    let resultado = parseFloat(primerNro * segundoNro);
+    console.log("El resultado de la multiplicacion es: " + resultado)
+} else if (operador == 4) {
+    let resultado = parseFloat(primerNro / segundoNro);
+    console.log("El resultado de la division es: " + resultado)
+} else {
+    console.log("Ingreso de manera incorrecta");
+}
