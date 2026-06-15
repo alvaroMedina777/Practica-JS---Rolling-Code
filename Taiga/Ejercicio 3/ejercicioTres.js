8071ecff112205f219addfa39e5712d421c1a900
Alvaro Medina <168005749+alvaroMedina777@users.noreply.github.com>
2026-06-15 19:46:14 -0300

    comienzo ejercicio tres

---
+++ Taiga/Ejercicio 3/ejercicioTres.js
@@ -0,0 +1,7 @@
+// 📝 Ejercicio 3: Calculadora básica
+// Solicita al usuario dos números decimales.  
+// Convierte las entradas. 
+// Pide al usuario que ingrese el tipo de operación: suma, resta, multiplicación o división.  
+// Usa condicionales para ejecutar la operación correspondiente.  
+// Muestra el resultado en la consola.  
+// Si la operación no es válida, informa al usuario con un mensaje en la consola.
