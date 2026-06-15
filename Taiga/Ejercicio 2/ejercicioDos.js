30f877e945ea7ca1a6323ce71d2c3b9290079d7b
Alvaro Medina <168005749+alvaroMedina777@users.noreply.github.com>
2026-06-15 19:35:50 -0300

    creo carpetas y archivos del ejercicio tres

---
+++ Taiga/Ejercicio 2/ejercicioDos.js
@@ -0,0 +1,14 @@
+// 📝 Ejercicio 2: Calcular promedio de notas
+// Pide al usuario tres notas con decimales.  
+// Convierte las entradas a números decimales.
+// Calcula el promedio de las tres notas usando operadores aritméticos.  
+// Muestra el promedio en la consola.  
+// Usa un condicional para indicar si el promedio es suficiente para aprobar (ejemplo: mayor o igual a 6).
+
+let nroUno = parseFloat(prompt("Ingres el primer numero decimal"));
+let nroDos = parseFloat(prompt("Ingrese el segundo numero decimal"));
+let nroTres = parseFloat(prompt("Ingrese el tercer numero decimal"));
+
+let promedio = (nroUno + nroDos + nroTres) / 3;
+
+console.log("Con la suma de los 3 numeros, el promedio es: " + promedio);
