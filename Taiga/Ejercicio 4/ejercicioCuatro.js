5c1d65ece3f7984f47a3f00be8e0a1db3c359479
Alvaro Medina <168005749+alvaroMedina777@users.noreply.github.com>
2026-06-22 18:04:26 -0300

    creo carpeta y archivos del ejercicio 4 de taiga

---
+++ Taiga/Ejercicio 4/ejercicioCuatro.js
@@ -0,0 +1,7 @@
+// Se necesita un script que solicite al usuario escribir una frase o texto y luego ingresar una palabra que quiera buscar. En cualquier caso (sea que encuentre la palabra o no) el programa debe devolver un mensaje informando el resultado (Usar emojis también).
+// Usar los métodos aprendidos en la clase para encontrar la solución.
+
+let menssage = prompt("Ingrese un mensaje saludando!");
+let palabra = prompt("Ingrese la palabra que desea buscar");
+
+
