edb9a69e941aff0e0b6c709a2de8911b668ae682
Alvaro Medina <168005749+alvaroMedina777@users.noreply.github.com>
2026-06-22 22:45:29 -0300

    creo carpeta y archivos del ejercicio 6

---
+++ Taiga/Ejercicio 6/ejercicioSeis.js
@@ -0,0 +1,7 @@
+// En un restaurante los clientes pueden pedir menú de carne, pescado o verdura. Si pide carne se le ofrecerá como bebida vino tinto, si pide pescado se le ofrecerá vino blanco y si pide verdura se le ofrecerá agua.
+
+// Si no elige el menú de la lista aparecerá la frase elija carne, pescado o verdura.
+
+let pedido = prompt("Que desea cenar? Tiene 3 opciones: 1) Carne, 2) Pescado 3) Verduras");
+
+switch ()
