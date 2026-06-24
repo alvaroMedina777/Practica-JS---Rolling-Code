aa5d83dbfcfd1881d1c1e834e646b39266fb5c48
Alvaro Medina <168005749+alvaroMedina777@users.noreply.github.com>
2026-06-23 23:20:27 -0300

    termino ejercicio sobre mostrar numeros utilizando el ciclo for

---
+++ Taiga/Ejercicio 7/ejercicioSiete.js
@@ -0,0 +1,8 @@
+// Mostrar números del 1 al 10. 
+// Crea un programa que muestre por consola los números del 1 al 10 utilizando un ciclo for. Salida esperada:
+
+
+for (let i = 0; i <= 10; i++) {
+    console.log("el numero es: " + i)
+}
+
