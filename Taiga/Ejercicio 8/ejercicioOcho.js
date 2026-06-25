2931494b6ceb0266d89cf105a26e1b26bdbbe28b
Alvaro Medina <168005749+alvaroMedina777@users.noreply.github.com>
2026-06-25 20:25:36 -0300

    termino ejercicio de tabla de multiplicar

--- Taiga/Ejercicio 8/ejercicioOcho.js
+++ Taiga/Ejercicio 8/ejercicioOcho.js
@@ -1,4 +1,16 @@
 // Solicita al usuario un número y muestra su tabla de multiplicar del 1 al 10 utilizando un ciclo for. Ejemplo:
 
-let numeroTabla = Number(prompt("Ingrese el numero de la tabla a multiplicar"));
+// ALGORITMO
+// Datos de entrada:
+//     Numero de tabla a multiplicar
+// Proceso:
+//     Calcular el numero ingresado por los numeros a multiplicar
+// Salida:
+//     Devolver el resultado
 
+let numeroTabla = Number(prompt("Ingrese el numero de la tabla a calcular"));
+
+for (let i = 1; i <= 10; i++) {
+    let resultado = numeroTabla * i;
+    console.log(`${numeroTabla} x ${i} = ${resultado}`)
+}
