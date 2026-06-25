827dd1e5edd6503c6861efb70d460ba51fa38e8e
Alvaro Medina <168005749+alvaroMedina777@users.noreply.github.com>
2026-06-25 20:12:31 -0300

    creo carpeta y archivo de ejercicio 8 del taiga

---
+++ Taiga/Ejercicio 8/ejercicioOcho.js
@@ -0,0 +1,4 @@
+// Solicita al usuario un número y muestra su tabla de multiplicar del 1 al 10 utilizando un ciclo for. Ejemplo:
+
+let numeroTabla = Number(prompt("Ingrese el numero de la tabla a multiplicar"));
+
