38b80943b2c4f2189ccba3c1c7140c93b0ffb31e
Alvaro Medina <168005749+alvaroMedina777@users.noreply.github.com>
2026-06-26 19:52:58 -0300

    comienzo ejercicio de acumulador de dinero

---
+++ Taiga/TAREA 4/Ejercicio 1/ejercicioUno.js
@@ -0,0 +1,18 @@
+// 1. El Acumulador de "Dinero con Tope" (Ciclo while)
+// Consigna: Imagina que quieres ahorrar para un objetivo de $1000. El programa debe pedir al usuario que ingrese montos de ahorro diarios (en un prompt o variable) y los vaya sumando. El ciclo while debe ejecutarse mientras la suma total sea menor a $1000. Al llegar o superar el objetivo, imprime "¡Objetivo alcanzado!".
+
+// Desafío lógico: ¿Qué pasa si el usuario ingresa un número negativo? Añade una validación para que no sume montos inválidos.
+
+// Objetivo: Manejo de acumuladores con condiciones de salida basadas en metas.
+
+
+// -- ALGORITMO --
+// DATOS DE ENTRADA
+//     Montos de ahorros
+// PROCESO
+//     Almacenar los ahorros
+//     Sumar los ahorros
+// DATOS DE SALIDA
+//     Mostrar la suma total y mensaje de "objetivo alcanzado"
+
+
