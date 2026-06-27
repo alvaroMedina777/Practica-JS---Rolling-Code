9cd410b265315950fad08fac624ee3ebe28d956b
Alvaro Medina <168005749+alvaroMedina777@users.noreply.github.com>
2026-06-26 22:07:56 -0300

    creo carpeta y archivos del ejercicio 2 de la tarea 4

---
+++ Taiga/TAREA 4/Ejercicio 2/ejercicioDos.js
@@ -0,0 +1,3 @@
+// 2. El "Triángulo de Asteriscos" (Ciclo for anidado)
+// Consigna: Utiliza dos bucles for (uno dentro de otro) para imprimir un triángulo de asteriscos en la consola con una altura de 5 líneas:
+
