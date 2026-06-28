fe1157abaefd853a6c1d9557d66b2067f5e09d84
Alvaro Medina <168005749+alvaroMedina777@users.noreply.github.com>
2026-06-28 18:32:41 -0300

    creo carpeta y archivos del ejercicio 5 de la tarea 4

---
+++ Taiga/TAREA 4/Ejercicio 5/ejercicioCinco.js
@@ -0,0 +1,13 @@
+// 5. Simulador de "Carrito de Descuentos Progresivos" (Ciclo while)
+// Contexto: Muchas tiendas online aplican descuentos que dependen de la cantidad de productos en el carrito.
+
+// Consigna: El usuario comienza con un producto. El programa debe preguntar el precio de cada producto y sumarlo. El ciclo while continuará pidiendo productos mientras el usuario escriba "si" para agregar otro. Si el total supera los $5000, el sistema debe imprimir: "¡Has obtenido un 10% de descuento!".
+
+// Desafío lógico: Debes llevar un control del total acumulado en cada iteración y aplicar el descuento solo una vez al final, antes de imprimir el resultado.
+
+// ALGORITMO 
+// -DATOS DE ENTRADA
+
+// -PROCESO 
+
+// -DATOS DE SALIDA
