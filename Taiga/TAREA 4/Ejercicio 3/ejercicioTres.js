cc8786b75bd182d820412c689511b5deb78bc72f
Alvaro Medina <168005749+alvaroMedina777@users.noreply.github.com>
2026-06-28 15:43:09 -0300

    creo carpeta y archivos del ejercicio 3 del token de seguridad

---
+++ Taiga/TAREA 4/Ejercicio 3/ejercicioTres.js
@@ -0,0 +1,7 @@
+// 3. El Simulador de "Token de Seguridad" (Ciclo while)
+// Contexto: Hoy en día, los sistemas de seguridad nos piden ingresar un código que caduca en segundos o nos limita la cantidad de intentos.
+
+// Consigna: Crea un programa que simule un sistema de autenticación de 2 pasos. El sistema tiene un código fijo (ej: 5555). El usuario tiene un máximo de 3 intentos para ingresarlo correctamente.
+
+// Desafío lógico: Usa un contador que aumente con cada intento fallido y un while que verifique dos condiciones: que el código sea incorrecto y que los intentos sean menores a 3.
+
