f9a2c1128381c8ef5af1ca02d9ec274697926a85
Alvaro Medina <168005749+alvaroMedina777@users.noreply.github.com>
2026-06-28 16:59:36 -0300

    Creo carpeta y archivos de ejercicio cuatro de la tarea 4

---
+++ Taiga/TAREA 4/Ejercicio 4/ejercicioCuatro.js
@@ -0,0 +1,19 @@
+// 4. Gestor de "Horas de Estudio" (Ciclo for)
+// Contexto: Los estudiantes suelen organizar su semana para cumplir con el curso Fullstack.
+
+// Consigna: Crea un programa donde el usuario ingrese cuántas horas dedicará a estudiar cada día de la semana (7 días). El programa debe usar un ciclo for para solicitar el dato 7 veces, ir sumando las horas en un acumulador y, al final, mostrar el promedio de horas diarias dedicadas al estudio.
+
+// Desafío lógico: ¿Qué pasa si el usuario ingresa un valor negativo? El programa debe ignorar ese dato y pedir que ingrese un número mayor a 0.
+
+// ALGORITMO
+// - DATOS DE ENTRADA
+//     Ingresar las hs de estudio (7 veces)
+// - PROCESO
+//     Sumar las horas
+//     Si ingresa un valor negativo, debe pedir que ingrese un numero mayor a 0
+// - DATOS DE SALIDA
+//     Mostrar promedio de horas diaras
+
+
+
+    
