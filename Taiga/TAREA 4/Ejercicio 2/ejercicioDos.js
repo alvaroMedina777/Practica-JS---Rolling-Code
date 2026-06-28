e190b7cbdd2e32723f81b232f0454736ff69685b
Alvaro Medina <168005749+alvaroMedina777@users.noreply.github.com>
2026-06-28 18:40:35 -0300

    continuo con el ejercicio 2 de la tarea 4

--- Taiga/TAREA 4/Ejercicio 2/ejercicioDos.js
+++ Taiga/TAREA 4/Ejercicio 2/ejercicioDos.js
@@ -1,3 +1,21 @@
 // 2. El "Triángulo de Asteriscos" (Ciclo for anidado)
 // Consigna: Utiliza dos bucles for (uno dentro de otro) para imprimir un triángulo de asteriscos en la consola con una altura de 5 líneas:
 
+// Desafío lógico: El bucle externo controla las filas y el interno controla cuántos asteriscos imprimir en cada fila. 
+
+// Objetivo: Dominar la complejidad de los bucles anidados, fundamental para entender estructuras de datos complejas en el futuro
+
+// ALGORITMO
+// -DATOS DE ENTRADA
+
+// -PROCESO 
+
+// -DATOS DE SALIDA
+
+let acumulador = 0;
+
+for (let i = 0; i = 5; i++) {
+    for (let ast = 0; ast = 0; ast++) {
+
+    }
+}
